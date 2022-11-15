c069e18dee0c824da85b2bb1540464a832e67f1d
NoorAlaaraj <Noor_alaaraj@outlook.com>
2022-11-15 11:22:50 +0300

    MinorChangeInSeeds

--- project-backend/seeds.js
+++ project-backend/seeds.js
@@ -10,15 +10,13 @@ Car.insertMany([
         Price: 'Price On Request',
         Description: 'Lamborghini Huracan | This is a smoke free Carfax certified 2015 Lamborghini Huracan LP610-4 coupe equipped with a 5.2L 610HP V10 engine and a 7-speed LDS dual-clutch with auto paddle shift transmission and AWD',
         Contact: '+973 34310225'
-        
-
     },
     {
         Photo:'',
         Brand: 'Bugatti',
         Year: '2019',
         Price: '3,819,854$',
-        Description: '2019 Bugati Chiron Sport Ultra rare sport | only 4k miles. We are an authorized dealer for Rimac part of the Bugatti Rimac',
+        Description: '2019 Bugati Chiron | Sport Ultra rare sport | only 4k miles. We are an authorized dealer for Rimac part of the Bugatti Rimac',
         Contact: '+973 33860444'
     },
     {
@@ -26,15 +24,15 @@ Car.insertMany([
         Brand: 'Ferrari',
         Year: '2020',
         Price:'381,469$',
-        Description: 'Ferrari 812 Superfast | Grigio Scuro                  This is what happens when you add a “Historical Color” to superfast high-tech material: your heart beats like a 812 times a minute. Especially when it has the appearance and luxury of this specific Ferrari: our 812 Superfast includes Carbon Racing Seats with Tricolore striping, a Suspension Lifter, JBL sound system, full carbon interior spec and 800 hp non-turbo-power on the rear wheels. Let us show you what a brilliant supercar looks like and come visit Holland superfast please'
-       
+        Description: 'Ferrari 812 Superfast | Grigio Scuro |This is what happens when you add a “Historical Color” to superfast high-tech material: your heart beats like a 812 times a minute. Especially when it has the appearance and luxury of this specific Ferrari: our 812 Superfast includes Carbon Racing Seats with Tricolore striping, a Suspension Lifter, JBL sound system, full carbon interior spec and 800 hp non-turbo-power on the rear wheels. Let us show you what a brilliant supercar looks like and come visit Holland superfast please',
+        Contact:'+973 37756671'
     },
     {
         Photo:'',
         Brand: 'Rolls-Royce',
         Year: '2019',
-        Price:'647,967$',
-        Description: 'Rolls-Royce Motor Cars Beverly Hills is pleased to present for sale this stunning 2019 Rolls-Royce Cullinan as part of our hand-selected pre-owned lineup. This gorgeous example is finished in the Black Diamond exterior paint and Creme Light interior / mileage 14K Mi / Engine: 12 Cylinder / Gearbox:Automatic /Fueltype:Petrol',
+        Price:'846,968$',
+        Description: 'Rolls-Royce | Motor Cars Beverly Hills is pleased to present for sale this stunning 2019 Rolls-Royce Cullinan as part of our hand-selected pre-owned lineup. This gorgeous example is finished in the Black Diamond exterior paint and Creme Light interior / mileage 14K Mi / Engine: 12 Cylinder / Gearbox:Automatic /Fueltype:Petrol',
         Contact:'+973 66622691'
     },
     {
